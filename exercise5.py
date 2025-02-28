bc131afc276f869413f1e206a563a8084558a2ff
adia e <estebaneadia@gmail.com>
2025-02-28 07:00:41 +0000

    menu

---
+++ exercise5.py
@@ -0,0 +1,8 @@
+menu = { "Pizza": 1.99, "Soda":  0.69, "Double Chunk Chocolate Chip Cookie": 2.49}
+
+def add_to_menu(item, price):
+    menu[item] = price
+
+add_to_menu("Hot Dog", 2.35)
+
+print(menu)
